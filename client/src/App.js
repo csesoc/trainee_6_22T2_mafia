5c741ca0e38f6161b35334d9a58e85db153166de
Linda Wang <lindawang@Lindas-MacBook.local>
2022-07-15 00:52:45 +1000

    implementing action menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import MainMenu from './components/MainMenu';
 import TurnSelectScreen from './components/TurnSelectScreen';
+import ActionMenu from './components/ActionMenu';
 
 function App() {
   return (
     <div>
       {/* <MainMenu/> */}
-      <TurnSelectScreen/>
+      {/* <TurnSelectScreen/> */}
+      <ActionMenu/>
     </div>
   );
 }
